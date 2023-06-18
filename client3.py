1ef1077128aac0e4c7b896d826c902b2ea93742b
Rajesh S <122197373+rajeshs098@users.noreply.github.com>
2023-06-18 18:48:16 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -61,3 +61,4 @@ if __name__ == "__main__":
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(price["ABC"], price["DEF"))
+         
